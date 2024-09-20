439ef928909068c627afff1576b67e59c7177602
alvesmariadefatima <mnunesalves334@gmail.com>
2024-09-20 14:45:22 -0300

    Arquivos de configuração para hospedagem da aplicação

---
+++ build_files.sh
@@ -0,0 +1,10 @@
+```sh
+#!/usr/bin/env bash
+
+echo “Migrando banco de dados…”
+python3 manage.py makemigrations — noinput
+python3 manage.py migrate — noinput
+
+echo “Coletando arquivos estáticos…”
+python3 manage.py collectstatic — noinput
+```
